f3addd699f686e990a147a483ece5bd7e4e0ad5c
Simon Meister <simon.meister@wagner.ch>
2025-09-08 14:59:30 +0200

    update boilerplate

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-NAME=$(python -c 'print(eval(open("package").read())["name"])')
+set -euo pipefail
+
+NAME=$(python3 -c 'print(eval(open("package").read())["name"])')
+VERSION=$(python3 -c 'print(eval(open("package").read())["version"])')
+WORKSPACE=$(pwd)
+
 rm /omd/sites/cmk/var/check_mk/packages/* ||:
 ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
 
-mkp -v pack $NAME
 
-# Set Outputs for GitHub Workflow steps
-if [ -n "$GITHUB_WORKSPACE" ]; then
-    echo "::set-output name=pkgfile::$(ls *.mkp)"
-    echo "::set-output name=pkgname::${NAME}"
-    VERSION=$(python -c 'print(eval(open("package").read())["version"])')
-    echo "::set-output name=pkgversion::$VERSION"
+mkp -v package package 2>&1 | sed '/Installing$/Q' ||:
+
+rm $NAME-$VERSION.mkp ||:
+cp /omd/sites/cmk/var/check_mk/packages_local/$NAME-$VERSION.mkp .
+
+mkp inspect $NAME-$VERSION.mkp
+
+# Set Outputs for GitHub Workflow steps (using environment files)
+if [ -n "${GITHUB_OUTPUT:-}" ]; then
+    {
+        echo "pkgfile=$(ls *.mkp)"
+        echo "pkgname=${NAME}"
+        echo "pkgversion=${VERSION}"
+    } >> "$GITHUB_OUTPUT"
 fi
